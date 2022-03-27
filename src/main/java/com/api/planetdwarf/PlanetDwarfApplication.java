8c88e64a7b30efeeb0ff59180f50a7db3c6d1357
Mario23junior <mariojunior3251@gmail.com>
2022-03-27 02:55:01 -0300

    Criando api de mapeamento de planetas anoes

---
+++ src/main/java/com/api/planetdwarf/PlanetDwarfApplication.java
@@ -0,0 +1,13 @@
+package com.api.planetdwarf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetDwarfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetDwarfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/planetdwarf/PlanetDwarfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.planetdwarf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetDwarfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
